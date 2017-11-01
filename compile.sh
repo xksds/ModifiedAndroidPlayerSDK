816f00ce460ffdc99f953ca43b96c7821dc25ee1
Arthur.Wang <arthur.wang@nio.com>
2017-11-01 11:20:39 +0800

    Modify compile.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 cd android
-./compile-ijk.sh $1
+
+if [ 0 -eq $# ];then
+	./compile-ijk.sh
+elif [ "clean" = $1 ] || [ "all" == $1 ] || [ "armv5" = $1 ] || [ "armv7a" = $1 ] || [ "arm64" = $1 ] || [ "x86" = $1 ] || [ "x86_64" = $1 ] ; then
+	./compile-ijk.sh $1
+else
+
+        echo "Usage:"
+        echo "  compile.sh armv5|armv7a|arm64|x86|x86_64"
+        echo "  compile.sh all|all32"
+        echo "  compile.sh all64"
+        echo "  compile.sh clean"
+
+fi
